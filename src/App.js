58e7db4106f2eba45b30cb19dda4e0e76d7f3a1a
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-31 15:19:49 +0200

    user-component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   
   return (
     <div className="App">
-       <Counter></Counter>
+       {/* <Counter></Counter> */}
+       {<ExternalUsers></ExternalUsers>}
+
+    </div>
+  );
+}
+
+function User(props){
+
+  return(
+    <div className='product'>
+      <h2>Name: {props.name}</h2>
+      <h3>Email: {props.email}</h3>
+    </div>
+  )
+}
+
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+  },[])
+  
+  return(
+    <div>
+      <h3>External Users</h3>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
     </div>
   );
 }
