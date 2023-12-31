b6db219552a087561a2b3f9cf4f89c0a7540d213
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-31 13:45:17 +0200

    counter-added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    {name:"Mobile", price:"15000"},
-    {name:"Mug", price:"150"},
-    {name:"Noodles", price:"18000"},
-    {name:"Pitzza", price:"100"},
-    {name:"Bodna", price:"100"},
-
-  ]
+  
   return (
     <div className="App">
-        {/* <Product name="Mobile" price="15000"></Product>
-        <Product name="Mug" price="1700"></Product>
-        <Product name="Noodles" price="1900"></Product>
-        <Product name="Pitzza" price="1600"></Product> */}
-
-        {
-          products.map(product => <Product name={product.name} price={product.price}></Product>)
-        }
+       <Counter></Counter>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  // console.log(count, setCount);
+
+  const handleIncrease = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+  
+  const handleDecrease = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  }
+  
+  return (
+    <div> 
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  )
 }
 
 function Product(props){
@@ -38,3 +48,21 @@ function Product(props){
 }
 
 export default App;
+
+//  {/* <Product name="Mobile" price="15000"></Product>
+//         <Product name="Mug" price="1700"></Product>
+//         <Product name="Noodles" price="1900"></Product>
+//         <Product name="Pitzza" price="1600"></Product> */}
+
+//         {
+//           products.map(product => <Product name={product.name} price={product.price}></Product>)
+//         }
+
+// const products = [
+//   {name:"Mobile", price:"15000"},
+//   {name:"Mug", price:"150"},
+//   {name:"Noodles", price:"18000"},
+//   {name:"Pitzza", price:"15000"},
+//   {name:"Bodna", price:"100"},
+
+// ]
